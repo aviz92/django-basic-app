5b10630311adf53d101ede120bac8ab2f1671f76
avizaguri-macbook <avi210892@gmail.com>
2026-02-06 13:23:53 +0200

    add first_app relationship to SecondAppSerializer; include optional slug field for related instance

--- django_basic_app/apps/second_app/serializers.py
+++ django_basic_app/apps/second_app/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 
 from .models import SecondApp
+from ..first_app.models import FirstApp
 
 
 class SecondAppSerializer(serializers.ModelSerializer):
     name = serializers.CharField(max_length=255, required=True)
+    first_app = serializers.SlugRelatedField(
+        queryset=FirstApp.objects.all(),
+        slug_field='name',
+        required=False,
+        allow_null=True,
+        help_text='name of the related FirstApp instance (optional)'
+    )
 
     class Meta:
         model = SecondApp
