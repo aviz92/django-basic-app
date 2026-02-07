3e9e6f287e322e87b3cde9b398598f7eec0869f8
avizaguri-macbook <avi210892@gmail.com>
2026-02-07 12:12:09 +0200

    chore: create index.html template and update index view for dynamic route rendering

--- django_basic_app/core/views.py
+++ django_basic_app/core/views.py
@@ -1,17 +1,19 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.urls import get_resolver
 
 
-def index(request):
-    resolver = get_resolver()
+def index(request: HttpRequest) -> HttpResponse:
     links = []
-
+    resolver = get_resolver()
     for pattern in resolver.url_patterns:
         route = str(pattern.pattern).replace('^', '').replace('$', '')
         if route and route not in ['admin/jsi18n/']:
             display_name = route.replace('/', '').replace('_', ' ').title()
-            links.append({
-                'route': f"/{route}",
-                'name': display_name or "Home"
-            })
+            links.append(
+                {
+                    'route': f"/{route}",
+                    'name': display_name or "Home"
+                }
+            )
     return render(request, 'index.html', {'links': links})
