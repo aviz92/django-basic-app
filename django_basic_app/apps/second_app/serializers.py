14609f31fbdf1af0bb4654f5fb0d3aea9204582a
avizaguri-macbook <avi210892@gmail.com>
2026-02-09 17:07:17 +0200

    chore: add explanation of first_app_name field on SecondApp to fetch the nested object via api call

--- django_basic_app/apps/second_app/serializers.py
+++ django_basic_app/apps/second_app/serializers.py
@@ -6,6 +6,11 @@ from .models import SecondApp
 
 class SecondAppSerializer(serializers.ModelSerializer):
     name = serializers.CharField(max_length=255, required=True)
+
+    # _name is added to fetch the nested object via api call and avoid conflict with the actual 'name' field of
+    # SecondApp and to make it clear that this field is related to the FirstApp model slug_field="name".
+    # The source="first_app" tells the serializer to use the 'first_app' relationship to fetch the related FirstApp
+    # instance based on the provided slug value.
     first_app_name = serializers.SlugRelatedField(
         source="first_app",
         queryset=FirstApp.objects.all(),
